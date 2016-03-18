24507176430f6ad2971ca31b658785bd5e052019
Rockfire <rocknfire@orange.fr>
2016-03-18 16:12:54 +0100

    Template Bootstrap 3 + Menu de base pour KNPMenu

---
+++ src/AppBundle/Menu/Builder.php
@@ -0,0 +1,24 @@
+<?php
+namespace AppBundle\Menu;
+
+use Knp\Menu\FactoryInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareTrait;
+
+class Builder implements ContainerAwareInterface
+{
+    use ContainerAwareTrait;
+
+    public function mainMenu(FactoryInterface $factory, array $options)
+    {
+        $menu = $factory->createItem('root');
+        $menu->setChildrenAttribute('class', 'nav navbar-nav');
+
+        $menu->addChild('Accueil', array('route' => 'homepage'))
+            ->setAttribute('icon', 'fa fa-list');
+        $menu->addChild('Connexion', array('route' => 'fos_user_security_login'));
+        $menu->addChild('Inscription', array('route' => 'fos_user_registration_register'));
+
+        return $menu;
+    }
+}
