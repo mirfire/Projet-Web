b4ffcca0c80ba0db04e7988de09a520d8a518fb4
Rockfire <rocknfire@orange.fr>
2016-03-21 15:55:42 +0100

    Menu différent selon si on est connecté ou pas

--- src/AppBundle/Menu/Builder.php
+++ src/AppBundle/Menu/Builder.php
@@ -17,8 +17,14 @@ class Builder implements ContainerAwareInterface
         $menu->addChild('Accueil', array('route' => 'homepage'))
             ->setAttribute('icon', 'fa fa-list');
         $menu->addChild('Recherche de Profil', array('route' => 'search'));
-        $menu->addChild('Connexion', array('route' => 'fos_user_security_login'));
-        $menu->addChild('Inscription', array('route' => 'fos_user_registration_register'));
+        // Si l'utilisateur est connecté est un
+        if($this->container->get('security.authorization_checker')->isGranted('ROLE_USER')) {
+            $menu->addChild('Mon Compte', array('route' => 'fos_user_profile_show'));
+        }
+        else {
+            $menu->addChild('Connexion', array('route' => 'fos_user_security_login'));
+            $menu->addChild('Inscription', array('route' => 'fos_user_registration_register'));
+        }
         $menu->addChild('A propos', array('route'=> 'about'));
         return $menu;
     }
