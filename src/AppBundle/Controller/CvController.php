773cf0493ad298c1b06a5d4031ea3cba85dc5d49
MSRog <lu.kermo@gmail.com>
2016-03-21 11:56:55 +0100

    creation controlleur page cv +page cv vierge

---
+++ src/AppBundle/Controller/CvController.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: link
+ * Date: 21/03/2016
+ * Time: 11:31
+ */
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+
+class CvControlleur
+
+
