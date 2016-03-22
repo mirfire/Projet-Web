3d8cdbab78c2004745de308457ba54cb17e339c6
Rockfire <rocknfire@orange.fr>
2016-03-22 18:46:51 +0100

    Expériences revues et corrigées

--- src/AppBundle/Controller/ExperienceController.php
+++ src/AppBundle/Controller/ExperienceController.php
@@ -1,6 +1,7 @@
 <?php
 namespace AppBundle\Controller;
-use AppBundle\Entity\Xp;
+
+use AppBundle\Entity\Experience;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -20,70 +21,100 @@ class ExperienceController extends Controller
     /**
      * @Route("/experience/add", name="user_experience_add")
      */
-     public function addAction(Request $request)
-     {
-         $Xp = new Xp();
-         $Xp->setUser($this->getUser());
-         $form_xp = $this->createFormBuilder($Xp)
-             ->add('name', TextType::class)
-             ->add('position', TextType::class)
-             ->add('company', TextType::class)
-             ->add('description', TextType::class)
-             ->add('location', TextType::class)
-             ->getForm();
-
-         $form_xp->handleRequest($request);
-         if ($form_xp->isSubmitted() && $form_xp->isValid()) {
-             $Xp = $form_xp->getData();
-             $em = $this->getDoctrine()->getManager();
-             $em->persist($Xp);
-             $em->flush();
-
-             return $this->redirectToRoute('user_xp');
-         }
-
-         return $this->render('userspace/experience/add.html.twig', array(
-             'form' => $form_xp->createView(),
-         ));
-     }
-    /**
-     * @Route("/experience/edit/{id}", name="user_experience_edit")
-     */
-    public function editAction(Request $request)
+    public function addAction(Request $request)
     {
-      $Xp = $product = $this->getDoctrine()
-          ->getRepository('AppBundle:Xp')
-          ->find($id);
-      $form_xp = $this->createFormBuilder($Xp)
-            ->add('name', TextType::class)
-            ->add('position', TextType::class)
-            ->add('company', TextType::class)
-            ->add('description', TextType::class)
-            ->add('location', TextType::class)
+        $Experience = new Experience();
+        $Experience->setUser($this->getUser());
+        $form = $this->createFormBuilder($Experience)
+            ->add('name', TextType::class, array(
+                'label' => 'Nom',
+            ))
+            ->add('position', TextType::class, array(
+                'label' => 'Poste',
+            ))
+            ->add('company', TextType::class, array(
+                'label' => 'Entreprise',
+            ))
+            ->add('description', TextType::class, array(
+                'label' => 'Description',
+            ))
+            ->add('location', TextType::class, array(
+                'label' => 'Endroit',
+            ))
+            ->add('save', SubmitType::class, array(
+                'label' => "Ajouter l'expérience"
+            ))
             ->getForm();
 
-            $form_xp->handleRequest($request);
-            if ($form_xp->isSubmitted() && $form_xp->isValid()) {
-                $Xp = $form_xp->getData();
-                $em = $this->getDoctrine()->getManager();
-                $em->persist($Xp);
-                $em->flush();
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $Experience = $form->getData();
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($Experience);
+            $em->flush();
 
             return $this->redirectToRoute('user_xp');
         }
 
         return $this->render('userspace/experience/add.html.twig', array(
-            'form' => $form_xp->createView(),
+            'form' => $form->createView(),
         ));
     }
+
+    /**
+     * @Route("/experience/edit/{id}", name="user_experience_edit")
+     */
+    public function editAction(Request $request, $id)
+    {
+        $Experience = $product = $this->getDoctrine()
+            ->getRepository('AppBundle:Experience')
+            ->find($id);
+        $form = $this->createFormBuilder($Experience)
+            ->add('name', TextType::class, array(
+                'label' => 'Nom',
+            ))
+            ->add('position', TextType::class, array(
+                'label' => 'Poste',
+            ))
+            ->add('company', TextType::class, array(
+                'label' => 'Entreprise',
+            ))
+            ->add('description', TextType::class, array(
+                'label' => 'Description',
+            ))
+            ->add('location', TextType::class, array(
+                'label' => 'Endroit',
+            ))
+            ->add('save', SubmitType::class, array(
+                'label' => "Éditer l'expérience"
+            ))
+            ->getForm();
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $Experience = $form->getData();
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($Experience);
+            $em->flush();
+
+            return $this->redirectToRoute('user_xp');
+        }
+
+        return $this->render('userspace/experience/add.html.twig', array(
+            'form' => $form->createView(),
+        ));
+    }
+
     /**
      * @Route("/experience/delete/{id}", name="user_experience_delete")
      */
-     public function deleteAction(Request $request, $id)
-     {
-         $xp = new Xp();
-         $em = $this->getDoctrine()->getManager();
-         $em->delete($xp);
-         $em->flush();
-     }
+    public function deleteAction(Request $request, $id)
+    {
+        $Experience = new Experience();
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($Experience);
+        $em->flush();
+
+        return $this->redirectToRoute('user_xp');
+    }
 }
