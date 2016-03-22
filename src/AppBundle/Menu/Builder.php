90d83d9b1fa46eeebcdb7b66b338388d25392486
Rockfire <rocknfire@orange.fr>
2016-03-22 09:28:48 +0100

    Finalisation du menu

--- src/AppBundle/Menu/Builder.php
+++ src/AppBundle/Menu/Builder.php
@@ -15,17 +15,37 @@ class Builder implements ContainerAwareInterface
         $menu->setChildrenAttribute('class', 'nav navbar-nav');
 
         $menu->addChild('Accueil', array('route' => 'homepage'))
-            ->setAttribute('icon', 'fa fa-list');
-        $menu->addChild('Recherche de Profil', array('route' => 'search'));
+            ->setAttribute('icon', 'fa fa-home');
+        $menu->addChild('Recherche de Profil', array('route' => 'search'))
+            ->setAttribute('icon', 'fa fa-search');
         // Si l'utilisateur est connecté est un
         if($this->container->get('security.authorization_checker')->isGranted('ROLE_USER')) {
-            $menu->addChild('Mon Compte', array('route' => 'fos_user_profile_show'));
+            $menu->addChild('Mon Compte', array('route' => 'fos_user_profile_show'))
+                ->setAttribute('dropdown', true)
+                ->setAttribute('icon', 'fa fa-user');
+            $menu['Mon Compte']->addChild('Profil', array('route' => 'fos_user_profile_show'))
+                ->setAttribute('icon', 'fa fa-user');
+            $menu['Mon Compte']->addChild('Mes Compétences', array('route' => /*'user_skills'*/ 'fos_user_profile_show'))
+                ->setAttribute('icon', 'fa fa-star');
+            $menu['Mon Compte']->addChild('Mes Projets', array('route' => /*'user_projects'*/ 'fos_user_profile_show'))
+                ->setAttribute('icon', 'fa fa-archive');
+            $menu['Mon Compte']->addChild('Mes Formations', array('route' => /*'user_courses'*/ 'fos_user_profile_show'))
+                ->setAttribute('icon', 'fa fa-graduation-cap');
+            $menu['Mon Compte']->addChild('Mes Expérience', array('route' => /*'user_experiences'*/ 'fos_user_profile_show'))
+                ->setAttribute('icon', 'fa fa-briefcase');
+            $menu['Mon Compte']->addChild('Déconnexion', array('route' => 'fos_user_security_logout'))
+                ->setAttribute('icon', 'fa fa-sign-in')
+                ->setAttribute('divider_prepend', true);
+
         }
         else {
-            $menu->addChild('Connexion', array('route' => 'fos_user_security_login'));
-            $menu->addChild('Inscription', array('route' => 'fos_user_registration_register'));
+            $menu->addChild('Connexion', array('route' => 'fos_user_security_login'))
+                ->setAttribute('icon', 'fa fa-sign-in');
+            $menu->addChild('Inscription', array('route' => 'fos_user_registration_register'))
+                ->setAttribute('icon', 'fa fa-user-plus');
         }
-        $menu->addChild('A propos', array('route'=> 'about'));
+        $menu->addChild('A propos', array('route'=> 'about'))
+            ->setAttribute('icon', 'fa fa-question-circle');
         return $menu;
     }
 }
